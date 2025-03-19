0f537aa9a57604a4188c27ea13d474dd16ba0216
DarkTrrex <miguelopez271105@gmail.com>
2025-03-19 03:24:37 +0100

    Tercera version de mi proyecto.Añadimos mucho mas contenido al fichero javaScript y modificamos el resto de documentos

--- Archivos/ScapeRoom.js
+++ Archivos/ScapeRoom.js
@@ -7,3 +7,31 @@ function Hola (nombre){
     }
  
 }
+function DeVuelta (){
+    window.location.assign("Bienvenida.html");/*Volver a la pestaña de Bienvenida*/
+}
+function Escena1 (){
+    let principal = document.getElementById("Principal");
+    principal.style.backgroundImage= "url('../Imagenes/Casper_y_Nova.jpg')";
+    principal.innerHTML = "";
+    let Volver = document.getElementById("Volver");
+    Volver.setAttribute("onclick","DeVuelta()");
+    document.getElementById("CuadroJuego").innerHTML = "Un dia 2 magos aburridos deciden tomar un viaje";
+}
+function Escena2 (){
+    let principal = document.getElementById("Principal");
+
+    principal.style.backgroundImage = "url('../Imagenes/galaxia.jpg')";
+    principal.style.backgroundSize = "cover";
+    
+
+    let nuevo = "<img id='Casper' src='../Imagenes/Casper.jpg' alt='Casper'/>" +
+                "<img id='Nova' src='../Imagenes/Nova.jpg' alt='Nova'/>";
+
+
+    principal.innerHTML = nuevo;
+    let Volver = document.getElementById("Volver");
+    Volver.setAttribute("onclick","Escena1()");
+    document.getElementById("CuadroJuego").innerHTML = "Uno de ellos que lleva un gorro morado se llama Casper mientras que la otra que lleva una gema en su cabeza se llama Nova";       
+    
+}
