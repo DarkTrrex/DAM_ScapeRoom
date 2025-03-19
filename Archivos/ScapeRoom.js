990b06d16f07a55558bd2dd520c3665d42fc0a66
Miguel <miguelopez271105@gmail.com>
2025-03-19 19:21:04 +0100

    Editando la escena3 y añadiendo la 4. Mejorando al redimension de los elementos

--- Archivos/ScapeRoom.js
+++ Archivos/ScapeRoom.js
@@ -8,30 +8,73 @@ function Hola (nombre){
  
 }
 function DeVuelta (){
+
     window.location.assign("Bienvenida.html");/*Volver a la pestaña de Bienvenida*/
 }
 function Escena1 (){
+
     let principal = document.getElementById("Principal");
+
     principal.style.backgroundImage= "url('../Imagenes/Casper_y_Nova.jpg')";
     principal.innerHTML = "";
+
+    document.getElementById("CuadroJuego").innerHTML = "Un dia 2 magos aburridos deciden tomar un viaje";
+
     let Volver = document.getElementById("Volver");
     Volver.setAttribute("onclick","DeVuelta()");
-    document.getElementById("CuadroJuego").innerHTML = "Un dia 2 magos aburridos deciden tomar un viaje";
+
+    let Seguir = document.getElementById("Seguir");
+    Seguir.setAttribute("onclick","Escena2()");
+
+    
 }
 function Escena2 (){
+
     let principal = document.getElementById("Principal");
+    let nuevo = "<img id='Casper' src='../Imagenes/Casper.jpg' alt='Casper'/>";
 
-    principal.style.backgroundImage = "url('../Imagenes/galaxia.jpg')";
-    principal.style.backgroundSize = "cover";
     
-    let nuevo = "<img id='Casper' src='../Imagenes/Casper.jpg' alt='Casper'/>" +
-                "<img id='Nova' src='../Imagenes/Nova.jpg' alt='Nova'/>";
-
     principal.innerHTML = nuevo;
+    principal.style.backgroundImage = "url('../Imagenes/galaxiaCasper.jpg')";
+    
+    document.getElementById("Casper").style.top ="9.5em";
+    document.getElementById("Casper").style.left ="40%";
+
+    document.getElementById("CuadroJuego").innerHTML = "Este es Casper. Lleva un gorro de dormir y a veces es muy precipitado";   
 
     let Volver = document.getElementById("Volver");
     Volver.setAttribute("onclick","Escena1()");
+
+    let Seguir = document.getElementById("Seguir");
+    Seguir.setAttribute("onclick","Escena3()");
+ 
+}
+function Escena3(){
+
+    let principal = document.getElementById("Principal");
+    let nuevo = "<img id='Nova' src='../Imagenes/Nova.jpg' alt='Nova'/>";
+
+    principal.innerHTML = nuevo;
+    principal.style.backgroundImage = "url('../Imagenes/galaxiaNova.jpg')";
+
+    document.getElementById("Nova").style.top= "10em";
+    document.getElementById("Nova").style.left="40%";
+    document.getElementById("CuadroJuego").innerHTML = "Esta es Nova. LLeva una gema en su frente y es mas prudente que Casper";
+
+    let Volver = document.getElementById("Volver");
+    Volver.setAttribute("onclick","Escena2()");
+
+    let Seguir = document.getElementById("Seguir");
+    Seguir.setAttribute("onclick","Escena4()");
+
     
-    document.getElementById("CuadroJuego").innerHTML = "Uno de ellos que lleva un gorro morado se llama Casper mientras que la otra que lleva una gema en su cabeza se llama Nova";       
+}
+function Escena4(){
+
+    let principal = document.getElementById("Pirncipal");
+    let nuevo = "<img id='Nova' src='../Imagenes/Nova.jpg' alt='Nova'/>" + "<img id='Casper' src='../Imagenes/Casper.jpg' alt='Casper'/>";
+
+    principal.innerHTML = nuevo;
+
     
 }
