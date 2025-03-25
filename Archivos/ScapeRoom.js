9ff91201de885281e382da31ddccd2180fd072a0
DarkTrrex <miguelopez271105@gmail.com>
2025-03-25 14:21:14 +0100

    Optimizacion de la pagina creando funciones para llamarlas y asi ahorrar mas lineas de codigo

--- Archivos/ScapeRoom.js
+++ Archivos/ScapeRoom.js
@@ -1,4 +1,5 @@
 function Hola (nombre){
+    
     if(nombre == "Nova"){
         document.getElementById("CuadroInicio").innerHTML= "Hola. Mi nombre es "+nombre+", vamos a vivir una increible aventura. Dale al boton de Start para comenzar!!!";
     }
@@ -7,109 +8,167 @@ function Hola (nombre){
     }
  
 }
+
 function DeVuelta (){
 
     window.location.assign("Bienvenida.html");/*Volver a la pestaña de Bienvenida*/
 }
-function Escena1 (){
+
+function BorrarContenido (){
+
+    let principal = document.getElementById("Principal");
+    principal.innerHTML = "";
+}
+
+function MeterContenido (nuevo,fondo){
 
     let principal = document.getElementById("Principal");
 
-    principal.style.backgroundImage= "url('../Imagenes/Casper_y_Nova.jpg')";
-    principal.innerHTML = "";
+    principal.innerHTML = nuevo;
+    principal.style.backgroundImage= "url("+fondo+")";
+    
+
+}
+
+function Casper (top,left,right,bottom){
+
+    casper = document.getElementById("Casper");
+    casper.style.top =top;
+    casper.style.left =left;
+    casper.style.right = right;
+    casper.style.bottom = bottom;
+}
+function Nova(top,left,right,bottom){
+
+    nova = document.getElementById("Nova");
+    nova.style.top =top;
+    nova.style.left =left;
+    nova.style.right = right;
+    nova.style.bottom = bottom;
+}
+
+function CambiarVolver (Escena,Imagen){
 
     let Volver = document.getElementById("Volver");
-    Volver.setAttribute("onclick","DeVuelta()");
+    Volver.setAttribute("onclick",Escena);
 
-    let Seguir = document.getElementById("Seguir");
-    Seguir.setAttribute("onclick","Escena2()");
-
-    document.getElementById("CuadroJuego").innerHTML = "Un dia 2 magos aburridos deciden tomar un viaje";
-
+    if(Imagen == ""){
+        Volver.setAttribute("src","../Imagenes/Volver.png");
+    }
+    else{
+        Volver.setAttribute("src",Imagen);
+    }
+    
+    
     
 }
+
+function CambiarSeguir (Escena,Imagen){
+    let Seguir = document.getElementById("Seguir");
+    Seguir.setAttribute("onclick",Escena);
+    
+    if(Imagen == ""){
+        Seguir.setAttribute("src","../Imagenes/Seguir.png");
+    }
+    else{
+        Seguir.setAttribute("src",Imagen);
+    }
+}
+
+function CambiarDialogo (Dialogo){
+
+    document.getElementById("CuadroJuego").innerHTML = Dialogo;
+
+}
+
+
+
+function Escena1 (){
+
+    BorrarContenido();
+
+    MeterContenido("",'../Imagenes/Casper_y_Nova.jpg');
+
+    CambiarVolver('DeVuelta()',"");
+
+    CambiarSeguir('Escena2()',"");
+
+    CambiarDialogo("Un dia 2 magos aburridos deciden tomar un viaje");
+    
+}
+
 function Escena2 (){
 
-    let principal = document.getElementById("Principal");
-    let nuevo = "<img id='Casper' src='../Imagenes/Casper.jpg' alt='Casper'/>";
+    BorrarContenido();
 
+    MeterContenido("<img id='Casper' src='../Imagenes/Casper.jpg' alt='Casper'/>",'../Imagenes/galaxiaCasper.jpg');
+
+    CambiarVolver('Escena1()',"");
+
+    CambiarSeguir('Escena3()',"");
+
+    CambiarDialogo("Este es Casper. Lleva un gorro de dormir y a veces es muy precipitado");
+
+    Casper("15%","42%","0","0");
     
-    principal.innerHTML = nuevo;
-    principal.style.backgroundImage = "url('../Imagenes/galaxiaCasper.jpg')";
-    
-    document.getElementById("Casper").style.top ="15%";
-    document.getElementById("Casper").style.left ="42%";
 
-    
-
-    let Volver = document.getElementById("Volver");
-    Volver.setAttribute("onclick","Escena1()");
-
-    let Seguir = document.getElementById("Seguir");
-    Seguir.setAttribute("onclick","Escena3()");
-
-    document.getElementById("CuadroJuego").innerHTML = "Este es Casper. Lleva un gorro de dormir y a veces es muy precipitado";   
+  
  
 }
 function Escena3(){
 
-    let principal = document.getElementById("Principal");
-    let nuevo = "<img id='Nova' src='../Imagenes/Nova.jpg' alt='Nova'/>";
+    BorrarContenido();
 
-    principal.innerHTML = nuevo;
-    principal.style.backgroundImage = "url('../Imagenes/galaxiaNova.jpg')";
+    MeterContenido("<img id='Nova' src='../Imagenes/Nova.jpg' alt='Nova'/>",'../Imagenes/galaxiaNova.jpg');
 
-    document.getElementById("Nova").style.top= "16%";
-    document.getElementById("Nova").style.left="42%";
+    CambiarVolver('Escena2()',"");
 
-    let Volver = document.getElementById("Volver");
-    Volver.setAttribute("onclick","Escena2()");
+    CambiarSeguir('Escena4()',"");
 
-    let Seguir = document.getElementById("Seguir");
-    Seguir.setAttribute("onclick","Escena4()");
+    CambiarDialogo("Esta es Nova. LLeva una gema en su frente y es mas prudente que Casper");
 
-    document.getElementById("CuadroJuego").innerHTML = "Esta es Nova. LLeva una gema en su frente y es mas prudente que Casper";
+    Nova("16%","42%","0","0");
+    
+
     
 }
 function Escena4(){
 
-    let principal = document.getElementById("Principal");
-    let nuevo = "<img id='Nova' src='../Imagenes/Nova.jpg' alt='Nova'/> <img id='Casper' src='../Imagenes/Casper.jpg' alt='Casper'/>";
     
-    principal.innerHTML = nuevo;
-    principal.style.backgroundImage="unset"; /*Pongo unset para quitarle el valor que tuviera*/
-    principal.style.backgroundColor = "black";
+    BorrarContenido();
 
-    let Casper = document.getElementById("Casper");
-    let Nova = document.getElementById("Nova");
+    MeterContenido("<img id='Nova' src='../Imagenes/Nova.jpg' alt='Nova'/> <img id='Casper' src='../Imagenes/Casper.jpg' alt='Casper'/>","");
 
-    Casper.style.top = "10%";
-    Casper.style.left="28%";
-    Nova.style.top = "12%";
-    Nova.style.left="25%";
+    CambiarVolver('Escena3()',"");
 
-    let Volver = document.getElementById("Volver");
-    Volver.setAttribute("onclick","Escena3()");
+    CambiarSeguir('Escena5()',"");
 
-    let Seguir = document.getElementById("Seguir");
-    Seguir.setAttribute("onclick","Escena5()");
-    Seguir.setAttribute("src","../Imagenes/Seguir.png");
+    CambiarDialogo("Tu les acompañaras en su viaje y les ayudaras a tomar las decisiones Necesarias. Buena Suerte");
 
-    let Consola = document.getElementById("CuadroJuego");
-    Consola.innerHTML= "Tu les acompañaras en su viaje y les ayudaras a tomar las decisiones Necesarias. Buena Suerte";
+    Casper("10%","30%","0","0");
+
+    Nova("12%","30%","0","0");
+
+    
+
+    
+
 }
 function Escena5 (){
-    let principal = document.getElementById("Principal");
     
-    principal.style.backgroundImage = "url('../Imagenes/Casa.jpg')";
-    principal.innerHTML = "";
-    principal.style.backgroundSize = "content";
-    
+    BorrarContenido();
 
-    let Añadirpuerta = "<div id='puerta' onclick ='Escena6()'></div>";
-    principal.innerHTML = Añadirpuerta;
+    MeterContenido("<div id='puerta' onclick ='Escena6()'></div>",'../Imagenes/Casa.jpg');
+
+    CambiarVolver('Escena4()',"");
+
+    CambiarSeguir("",'../Imagenes/NoSeguir.png');
+
+    CambiarDialogo("Esta es la casa de Casper y Nova"+" <br></br>"+" (Pulsa la puerta para entrar)");
+
+    document.getElementById("Principal").style.backgroundSize = "content";
+    
    
-
     let puerta = document.getElementById("puerta");
     puerta.style.width = "6%";
     puerta.style.height = "10%";
@@ -121,97 +180,75 @@ function Escena5 (){
     puerta.style.position = "relative";
     puerta.style.top = "71%";
     puerta.style.left = "57%";
-    
-
-
-
-    let Volver = document.getElementById("Volver");
-    Volver.setAttribute("onclick","Escena4()");
-
-    let Seguir = document.getElementById("Seguir");
-    Seguir.setAttribute("onclick","");
-    Seguir.setAttribute("src","../Imagenes/NoSeguir.png");
-
-    document.getElementById("CuadroJuego").innerHTML="Esta es la casa de Casper y Nova"+" <br></br>"+" (Pulsa la puerta para entrar)";
 
 }
 function Escena6 (){
-    let principal = document.getElementById("Principal");
-    let nuevo = "<img id='Nova' src='../Imagenes/Nova.jpg' alt='Nova'/>";
-    
-    principal.innerHTML = nuevo;
-    principal.style.backgroundImage ="url('../Imagenes/Salon.jpg')";
 
-   
-    let Nova = document.getElementById("Nova");
+    BorrarContenido();
 
-    Nova.style.top = "35%";
-    Nova.style.left="35%";
+    MeterContenido("<img id='Nova' src='../Imagenes/Nova.jpg' alt='Nova'/>",'../Imagenes/Salon.jpg');
 
+    CambiarVolver('Escena5()',"");
 
+    CambiarSeguir("Escena7()","");
 
-    let Volver = document.getElementById("Volver");
-    Volver.setAttribute("onclick","Escena5()");
+    CambiarDialogo("Nova: Casper date prisa o no llegaremos para coger sitio en el Motel!!!");
 
-    let Seguir = document.getElementById("Seguir");
-    Seguir.setAttribute("onclick","Escena7()");
-    Seguir.setAttribute("src","../Imagenes/Seguir.png");
-
-    document.getElementById("CuadroJuego").innerHTML="Nova: Casper date prisa o no llegaremos para coger sitio en el Motel!!!";
-
+    Nova("35%","35%","0","0");
 
 }
 function Escena7 (){
-    let principal = document.getElementById("Principal");
-    let nuevo = "<img id='Nova' src='../Imagenes/Nova.jpg' alt='Nova'/> "+"<div id='puerta' onclick ='Escena8()'></div>";
-    
-    principal.innerHTML = nuevo;
-    principal.style.backgroundImage ="url('../Imagenes/Salon.jpg')";
+
+    BorrarContenido();
+
+    MeterContenido("<img id='Nova' src='../Imagenes/Nova.jpg' alt='Nova'/> "+"<div id='puerta' onclick ='Escena8()'></div>",'../Imagenes/Salon.jpg');
+
+    CambiarVolver('Escena6()',"");
+
+    CambiarSeguir("","../Imagenes/NoSeguir.png");
+
+    CambiarDialogo("Casper: Ya voy!!! "+"<br></br> "+"(Pulsa la puerta de la entrada para salir)");
+
+    Nova("35%","35%","0","0");
 
     let puerta = document.getElementById("puerta");
     puerta.style.width = "30%";
     puerta.style.height = "60%";
     puerta.style.bottom = "30%";
     puerta.style.position = "relative";
-    Nova.style.top = "35%";
-    Nova.style.left="35%";
-
-    let Volver = document.getElementById("Volver");
-    Volver.setAttribute("onclick","Escena6()");
-
-    let Seguir = document.getElementById("Seguir");
-    Seguir.setAttribute("onclick","");
-    Seguir.setAttribute("src","../Imagenes/NoSeguir.png");
-
-    document.getElementById("CuadroJuego").innerHTML="Casper: Ya voy!!! "+"<br></br> "+"(Pulsa la puerta de la entrada para salir)";
     
 }
-function Escena8 (){
+function Escena8 (){  
     
-    let principal = document.getElementById("Principal");
-    let nuevo = "<img id='Nova' src='../Imagenes/Nova.jpg' alt='Nova'/> <img id='Casper' src='../Imagenes/Casper.jpg' alt='Casper'/>";
+    BorrarContenido();
 
-    principal.style.backgroundImage ="url('../Imagenes/Casa.jpg')";
-    principal.innerHTML = nuevo;
+    MeterContenido("<img id='Nova' src='../Imagenes/Nova.jpg' alt='Nova'/> <img id='Casper' src='../Imagenes/Casper.jpg' alt='Casper'/>",'../Imagenes/Casa.jpg');
 
-    let Nova = document.getElementById("Nova");
-    let Casper = document.getElementById("Casper");
+    CambiarVolver('Escena7()',"");
 
-    Nova.style.top = "52%";
-    Nova.style.left="3%";
+    CambiarSeguir("Escena9()","../Imagenes/BusDia.png");
 
-    Casper.style.top = "49%";
-    Casper.style.left="3%";
+    CambiarDialogo("Nova:Bien. Ya estamos listos para la aventura" +" <br></br> "+"(Pulsa el bus para continuar)");
 
-    let Volver = document.getElementById("Volver");
-    Volver.setAttribute("onclick","Escena7()");
-
-    let Seguir = document.getElementById("Seguir");
-    Seguir.setAttribute("onclick","Escena9()");
-    Seguir.setAttribute("src","../Imagenes/Bus.png");
-
-    document.getElementById("CuadroJuego").innerHTML="Nova:Bien. Ya estamos listos para la aventura" +" <br></br> "+"(Pulsa el bus para continuar)";
+    Nova("52%","3%","0","0");
+   
+    Casper("49%","3%","0","0");
+   
 }
 function Escena9(){
-    
+     
+    BorrarContenido();
+
+    MeterContenido("<img id='Nova' src='../Imagenes/Nova.jpg' alt='Nova'/> <img id='Casper' src='../Imagenes/Casper.jpg' alt='Casper'/>",'../Imagenes/Casa.jpg');
+
+    CambiarVolver('Escena8()',"../Imagenes/BusNoche.png");
+
+    CambiarSeguir("Escena10()","");
+
+    CambiarDialogo("");
+
+    Nova("52%","3%","0","0");
+   
+    Casper("49%","3%","0","0");
+   
 }
